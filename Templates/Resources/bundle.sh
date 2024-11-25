fcfdd430e0d2c5becf35639292dc7ad4796d09aa
byteproject <stefan@8-bit.info>
2024-11-25 15:13:07 +0100

    add full support for deprecated target, improved cleanup

--- Templates/Resources/bundle.sh
+++ Templates/Resources/bundle.sh
@@ -8,7 +8,7 @@
 #read config file 
 source config.sh
 
-echo "bundle.sh 2.1 - the game release archiver"
+echo "bundle.sh 2.2 - the game release archiver"
 echo -e "Puny BuildTools, (c) 2024 Stefan Vogt\n"
 
 while getopts ':t:h' opt; do
@@ -22,34 +22,67 @@ while getopts ':t:h' opt; do
         cp Releases/readme.txt .
         cp Releases/licenses.txt .
         cp Releases/game.transcript .
-        cp ~/FictionTools/Templates/Interpreters/Pro-DOS-v2.dsk .
+        cp ~/FictionTools/Templates/Interpreters/ProDOS_SAM.dsk .
         cp ~/FictionTools/Templates/Interpreters/CPM_Plus_speccy.dsk .
         cp -R Releases/DOS .
+        if [ -f ${STORY}_dragon64.vdk ] ; then
+            cp ~/FictionTools/Templates/Interpreters/dragon64_loader.vdk .
+        fi
       
-        # by standard all possible z5/multi-z targets are enabled, 
-        # all z3-only targets are disabled (see below)
-        zip -r ${STORY}_${RELEASE}.zip ${STORY}_apple2_s1.dsk ${STORY}_apple2_s2.dsk ${STORY}_speccy.dsk ${STORY}_amiga.adf ${STORY}_atari8bit.atr ${STORY}_c128.d71 ${STORY}_plus4.d64 ${STORY}_c64.d64 ${STORY}_mega65.d81 ${STORY}_cpc_pcw.dsk ${STORY}_atarist.st ${STORY}.z5 ${STORY}_bbc_elk.ssd ${STORY}_MSX.dsk CPM_Plus_speccy.dsk ${STORY}_mac.dsk ${STORY}_sam_coupe.cpm Pro-DOS-v2.dsk PlayIF.pdf readme.txt licenses.txt game.transcript DOS
+        # bundle disk images
+        zip -r ${STORY}_${RELEASE}.zip ${STORY}_apple2_s1.dsk ${STORY}_apple2_s2.dsk ${STORY}_speccy.dsk ${STORY}_amiga.adf ${STORY}_atari8bit.atr ${STORY}_c128.d71 ${STORY}_plus4.d64 ${STORY}_c64.d64 ${STORY}_mega65.d81 ${STORY}_cpc_pcw.dsk ${STORY}_atarist.st ${STORY}.z5 ${STORY}_bbc_elk.ssd ${STORY}_MSX.dsk CPM_Plus_speccy.dsk ${STORY}_mac.dsk ${STORY}_sam_coupe.cpm ProDOS_SAM.dsk PlayIF.pdf readme.txt licenses.txt game.transcript DOS
 
+        # in case you also build a target with the hidden -b c128_d64.sh switch 
         if [ -f ${STORY}_c128.d64 ] ; then
             zip ${STORY}_${RELEASE}.zip ${STORY}_c128.d64
         fi
+        # TRS80 Model 3 and 4 are optional targets, as they require Wine32, which 
+        # won't work for instance on MacOS, when using the BuildTools with Orbstack
         if [ -f ${STORY}_trs80_m3.dsk ] ; then
             zip ${STORY}_${RELEASE}.zip ${STORY}_trs80_m3.dsk
         fi
         if [ -f ${STORY}_trs80_m4.dsk ] ; then
             zip ${STORY}_${RELEASE}.zip ${STORY}_trs80_m4.dsk
         fi
+        # Z-machine version 3 only targets (deprecated) start here
+        if [ -f ${STORY}.z3 ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}.z3
+        fi
+        if [ -f ${STORY}_dragon64.vdk ] ; then
+            zip ${STORY}_${RELEASE}.zip dragon64_loader.vdk
+            zip ${STORY}_${RELEASE}.zip ${STORY}_dragon64.vdk
+        fi
+        if [ -f ${STORY}_ti99.dsk ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}_ti99.dsk
+        fi
+        if [ -f ${STORY}_oric_1.dsk ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}_oric_1.dsk
+            zip ${STORY}_${RELEASE}.zip ${STORY}_oric_2.dsk
+        fi
+        if [ -f ${STORY}_vic20_pet.d64 ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}_vic20_pet.d64
+        fi
+        if [ -f ${STORY}_trs_coco.dsk ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}_trs_coco.dsk
+        fi
+        if [ -f ${STORY}_osborne1.cpm ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}_osborne1.cpm
+        fi
+        if [ -f ${STORY}_kaypro.cpm ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}_kaypro.cpm
+        fi
+        if [ -f ${STORY}_decrainbow.cpm ] ; then
+            zip ${STORY}_${RELEASE}.zip ${STORY}_decrainbow.cpm
+        fi
 
-        # these targets only support z3 and are deactivated by default, 
-        # add them to the zip line above if desired
-        # ${STORY}.z3 ${STORY}_ti99.dsk ${STORY}_oric_1.dsk ${STORY}_oric_2.dsk ${STORY}_vic20_pet.d64 ${STORY}_trs_coco.dsk ${STORY}_dragon64.vdk dragon64_loader.vdk ${STORY}_osborne1.cpm ${STORY}_kaypro.cpm ${STORY}_decrainbow.cpm
-
+        #cleanup
         rm PlayIF.pdf
         rm readme.txt
         rm licenses.txt
         rm game.transcript
         rm CPM_Plus_speccy.dsk
-        rm Pro-DOS-v2.dsk
+        rm dragon64_loader.vdk
+        rm ProDOS_SAM.dsk
         cp ${STORY}_${RELEASE}.zip ${OPTARG}
         rm -rf DOS
         rm ${STORY}_${RELEASE}.zip
