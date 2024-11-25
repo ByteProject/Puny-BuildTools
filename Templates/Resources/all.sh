83bbfcef79637896511a76509a230b40d1e4914d
byteproject <stefan@8-bit.info>
2024-11-25 15:29:05 +0100

    adding -d flag to support deprecated targets

--- Templates/Resources/all.sh
+++ Templates/Resources/all.sh
@@ -2,8 +2,32 @@
 # all.sh  - local script to build all targets of your project
 # Puny BuildTools, (c) 2024 Stefan Vogt
 
-# Z-machine version 5 systems (standard config)
-puny -b c64 -b apple2 -b bbc_acorn -b speccy -b plus4 -b macintosh -b atari_st -b mega65 -b msx -b trs80_m3 -b trs80_m4 -b c128 -b sam_coupe -b cpc_pcw -b dos -b amiga -b a8bit
+# By standard, all.sh will only build the recommended disk images. You can
+# force building deprecated systems using the -d switch, e.g. ./all.sh -d
 
-# Z-machine version 3 only systems (enable if desired)
-# puny -b decrainbow -b kaypro -b osborne1 -b trs_coco_dragon64 -b vic20_pet -b oric -b ti99 
+main()
+{
+	printf "all.sh - building disk images [...]\n"
+	# Z-machine version 5 systems (recommended)
+    puny -b c64 -b apple2 -b bbc_acorn -b speccy -b plus4 -b macintosh -b atari_st -b mega65 -b msx -b trs80_m3 -b trs80_m4 -b c128 -b sam_coupe -b cpc_pcw -b dos -b amiga -b a8bit
+}
+
+deprecated()
+{
+	printf "\nall.sh - building deprecated disk images [...]\n"
+    # Z-machine version 3 only systems (deprecated)
+    puny -b decrainbow -b kaypro -b osborne1 -b trs_coco_dragon64 -b vic20_pet -b oric -b ti99
+}
+
+# Always run main but check for -d flag
+main
+
+while getopts d opts
+do
+	case $opts in
+		d) deprecated
+		;;
+	esac
+done
+
+exit 0
